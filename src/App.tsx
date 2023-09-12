fb1bc102a4e7561b0cc0b69a88258a6695568433
Henrique Batista <wellington.h.batista@hotmail.com>
2023-09-11 21:46:21 -0300

    Cria o grid da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Separator } from './components/ui/separator'
 
 export function App() {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <div className="px-6 py-3 flex items-center justify-between border-b">
         <h1 className="text-xl font-bold">Upload AI</h1>
 
@@ -19,6 +19,19 @@ export function App() {
           </Button>
         </div>
       </div>
+
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1"></div>
+          <p className="text-sm text-muted-forground">
+            {' '}
+            Lembre-se: você pode utilizar a variável transcription no seu prompt
+            para adicionar o conteúdo da trancrição do video selecionado
+          </p>
+        </div>
+
+        <aside className="w-80"></aside>
+      </main>
     </div>
   )
 }
